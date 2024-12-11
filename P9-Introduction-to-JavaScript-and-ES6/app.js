6c9aa3e2e5fec453cfd5ead2586dac6688ec1823
Abdullah Qaid Mu'aadz <107825990+MIM10@users.noreply.github.com>
2024-12-11 14:13:17 +0700

    update

--- P9-Introduction-to-JavaScript-and-ES6/app.js
+++ P9-Introduction-to-JavaScript-and-ES6/app.js
@@ -1,20 +1,11 @@
 const { index, store, update, destroy } = require('./fruitController');
 
 const main = () => {
-    console.log('Method index - Menampilkan buah');
-    console.log(index());
-
-    console.log('\nMethod store - Menambahkan buah Strawberry');
+    console.log(`Method index - Menampilkan buah`);
+    index();
     store('Strawberry');
-    console.log(index());
-
-    console.log('\nMethod update - Update data 0 menjadi Kedondong');
     update(0, 'Kedondong');
-    console.log(index());
-
-    console.log('\nMethod destroy - Menghapus data 0');
     destroy(0);
-    console.log(index());
 };
 
 main();
--- P9-Introduction-to-JavaScript-and-ES6/fruitController.js
+++ P9-Introduction-to-JavaScript-and-ES6/fruitController.js
@@ -1,23 +1,31 @@
-const fruits = require('./fruits');
+const fruits = require(`./fruits`);
 
 const index = () => {
-    return fruits;
+    for (const fruit of fruits) {
+        console.log(`-  ${fruit}`);
+    }
 }
 
 const store = (name) => {
+    console.log(`\nMethod store - Menambahkan buah ${name}`);
     fruits.push(name);
+    index();
 }
 
 const update = (position, name) => {
+    console.log(`\nMethod update - Update data ${position} menjadi ${name}`);
     if (isValidPosition(position)) {
         fruits[position] = name;
     }
+    index();
 }
 
 const destroy = (position) => {
+    console.log(`\nMethod destroy - Menghapus data ${position}`);
     if (isValidPosition(position)) {
         fruits.splice(position, 1);
     }
+    index();
 }
 
 const isValidPosition = (position) => {
